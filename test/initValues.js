eb602f75e027b8b8dd623ebdd31371281fca202a
Markus Iivonen <markus.iivonen@metropolia.fi>
2014-12-14 19:55:53 +0200

    added tests

--- test/initValues.js
+++ test/initValues.js
@@ -7,3 +7,14 @@ exports['checkValues'] = function (test) {
     test.notEqual(testUserConfig.local.storage.length,0);
     test.done();
 };
+
+exports['checkInit'] = function (test) {
+  stream.initConfig();
+  test.equal(stream.getStorage(),testUserConfig.local.storage);
+  test.deepEqual(stream.getConnection(),{
+    "apikey": testUserConfig.twitter.apiKey,
+    "stream" : testUserConfig.twitter.streamType.basic
+  });
+
+  test.done();
+};
