574fb545713781677daac99ec675d83b83d869f7
Markus Iivonen <markus.iivonen@metropolia.fi>
2014-12-21 18:55:37 +0200

    updated tests

--- test/initValues.js
+++ test/initValues.js
@@ -2,9 +2,12 @@ var stream = require('../lib/twitter-stream.js');
 var testUserConfig = require('../user-config.json');
 
 exports['checkValues'] = function (test) {
-    test.notEqual(testUserConfig.twitter.apiKey.length,0);
-    test.notEqual(testUserConfig.twitter.streamType.basic.length,0);
+    test.notEqual(testUserConfig.twitter.consumer_key.length,0);
+    test.notEqual(testUserConfig.twitter.consumer_secret.length,0);
+    test.notEqual(testUserConfig.twitter.access_token_key.length,0);
+    test.notEqual(testUserConfig.twitter.access_token_secret.length,0);
     test.notEqual(testUserConfig.local.storage.length,0);
+    test.notEqual(testUserConfig.local.maxSize.length,0);
     test.done();
 };
 
@@ -12,7 +15,10 @@ exports['checkInit'] = function (test) {
   stream.initConfig();
   test.equal(stream.getStorage(),testUserConfig.local.storage);
   test.deepEqual(stream.getConnection(),{
-    "apikey": testUserConfig.twitter.apiKey,
+    "consumer_key": testUserConfig.twitter.consumer_key,
+    "consumer_secret": testUserConfig.twitter.consumer_secret,
+    "access_token_key": testUserConfig.twitter.access_token_key,
+    "access_token_secret": testUserConfig.twitter.access_token_secret,
     "stream" : testUserConfig.twitter.streamType.basic,
     "optionalStream": testUserConfig.twitter.streamType.userSpecific
   });
